e7f98c1bebe9f20612e4612761cc2a054e448e08
PIETRO MACORI s283421 <s283421@studenti.polito.it>
2021-07-01 15:44:12 +0200

    fix defs.mk v2

--- defs.mk
+++ defs.mk
@@ -13,7 +13,7 @@
 # The things that can be set here are documented in mk/os161.config.mk.
 #
 
-OSTREE=$HOME/os161/root
+OSTREE=$(HOME)/os161/root
 PLATFORM=sys161
 MACHINE=mips
 COMPAT_CFLAGS= -DNEED_NTOHLL
