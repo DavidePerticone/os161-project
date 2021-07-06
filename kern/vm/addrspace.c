3c027f77afcc3a77b4cd7929f706e93f388a95f2
Davide Perticone <s281633@studenti.polito.it>
2021-07-06 11:20:04 +0200

    "Implemented partial demand loading - BUG VOP_READ a page"

--- kern/vm/addrspace.c
+++ kern/vm/addrspace.c
@@ -52,7 +52,8 @@ static unsigned long *allocSize = NULL;
 static int nRamFrames = 0;
 static int allocTableActive = 0;
 
-static int isTableActive() {
+static int isTableActive()
+{
   int active;
   spinlock_acquire(&freemem_lock);
   active = allocTableActive;
@@ -60,67 +61,78 @@ static int isTableActive() {
   return active;
 }
 
-void
-vm_bootstrap(void)
+void vm_bootstrap(void)
 {
   int i;
   paddr_t firstpaddr, addr;
   int occupiedpages, freepages;
-  nRamFrames = ((int)ram_getsize())/PAGE_SIZE; 
+  nRamFrames = ((int)ram_getsize()) / PAGE_SIZE;
   kprintf("ram_getsize(): %d\n", (int)ram_getsize());
   kprintf("RamFrames: %d\n", nRamFrames);
   /* alloc inverted page table */
-  if(create_ipt()==-1) return;
-  /* alloc freeRamFrame and allocSize */  
-  freeRamFrames = kmalloc(sizeof(unsigned char)*nRamFrames);
-  if (freeRamFrames==NULL) return;  
-  allocSize     = kmalloc(sizeof(unsigned long)*nRamFrames);
-  if (allocSize==NULL) {    
+  if (create_ipt() == -1)
+    return;
+  /* alloc freeRamFrame and allocSize */
+  freeRamFrames = kmalloc(sizeof(unsigned char) * nRamFrames);
+  if (freeRamFrames == NULL)
+    return;
+  allocSize = kmalloc(sizeof(unsigned long) * nRamFrames);
+  if (allocSize == NULL)
+  {
     /* reset to disable this vm management */
-    freeRamFrames = NULL; return;
+    freeRamFrames = NULL;
+    return;
   }
-  for (i=0; i<nRamFrames; i++) {    
+  for (i = 0; i < nRamFrames; i++)
+  {
     freeRamFrames[i] = (unsigned char)0;
-    allocSize[i]     = 0;  
+    allocSize[i] = 0;
   }
   spinlock_acquire(&freemem_lock);
   allocTableActive = 1;
   spinlock_release(&freemem_lock);
   /*allocation and deallocation of all ram to avoid using ram_stealmem*/
   firstpaddr = ram_getfirstfreeafterbootstrap(); /* get address of first free page */
-  occupiedpages = ((int)firstpaddr)/PAGE_SIZE; /* calculate occupied pages by kernel */
-  freepages = nRamFrames-occupiedpages; /* calculate free pages remaining*/
-  addr = alloc_kpages(freepages); /*allocate all pages available*/
-  free_kpages(addr); /* deallocate all pages previously allocated */
-
+  occupiedpages = ((int)firstpaddr) / PAGE_SIZE; /* calculate occupied pages by kernel */
+  freepages = nRamFrames - occupiedpages;        /* calculate free pages remaining*/
+  addr = alloc_kpages(freepages);                /*allocate all pages available*/
+  free_kpages(addr);                             /* deallocate all pages previously allocated */
 }
 
-static paddr_t 
-getfreeppages(unsigned long npages) {
-  paddr_t addr;	
+static paddr_t
+getfreeppages(unsigned long npages)
+{
+  paddr_t addr;
   long i, first, found, np = (long)npages;
 
-  if (!isTableActive()) return 0; 
+  if (!isTableActive())
+    return 0;
   spinlock_acquire(&freemem_lock);
-  for (i=0,first=found=-1; i<nRamFrames; i++) {
-    if (freeRamFrames[i]) {
-      if (i==0 || !freeRamFrames[i-1]) 
-        first = i; /* set first free in an interval */   
-      if (i-first+1 >= np) {
+  for (i = 0, first = found = -1; i < nRamFrames; i++)
+  {
+    if (freeRamFrames[i])
+    {
+      if (i == 0 || !freeRamFrames[i - 1])
+        first = i; /* set first free in an interval */
+      if (i - first + 1 >= np)
+      {
         found = first;
         break;
       }
     }
   }
-	
-  if (found>=0) {
-    for (i=found; i<found+np; i++) {
+
+  if (found >= 0)
+  {
+    for (i = found; i < found + np; i++)
+    {
       freeRamFrames[i] = (unsigned char)0;
     }
     allocSize[found] = np;
-    addr = (paddr_t) found*PAGE_SIZE;
+    addr = (paddr_t)found * PAGE_SIZE;
   }
-  else {
+  else
+  {
     addr = 0;
   }
 
@@ -129,17 +141,17 @@ getfreeppages(unsigned long npages) {
   return addr;
 }
 
-
 static void
 vm_can_sleep(void)
 {
-	if (CURCPU_EXISTS()) {
-		/* must not hold spinlocks */
-		KASSERT(curcpu->c_spinlocks == 0);
+  if (CURCPU_EXISTS())
+  {
+    /* must not hold spinlocks */
+    KASSERT(curcpu->c_spinlocks == 0);
 
-		/* must not be in an interrupt handler */
-		KASSERT(curthread->t_in_interrupt == 0);
-	}
+    /* must not be in an interrupt handler */
+    KASSERT(curthread->t_in_interrupt == 0);
+  }
 }
 
 static paddr_t
@@ -149,33 +161,37 @@ getppages(unsigned long npages)
 
   /* try freed pages first */
   addr = getfreeppages(npages);
-  if (addr == 0) {
+  if (addr == 0)
+  {
     /* call stealmem */
     spinlock_acquire(&stealmem_lock);
     addr = ram_stealmem(npages);
     spinlock_release(&stealmem_lock);
   }
-  if (addr!=0 && isTableActive()) {
+  if (addr != 0 && isTableActive())
+  {
     spinlock_acquire(&freemem_lock);
-    allocSize[addr/PAGE_SIZE] = npages;
+    allocSize[addr / PAGE_SIZE] = npages;
     spinlock_release(&freemem_lock);
-  } 
+  }
 
   return addr;
 }
 
+static int
+freeppages(paddr_t addr, unsigned long npages)
+{
+  long i, first, np = (long)npages;
 
-static int 
-freeppages(paddr_t addr, unsigned long npages){
-  long i, first, np=(long)npages;	
-
-  if (!isTableActive()) return 0; 
-  first = addr/PAGE_SIZE;
-  KASSERT(allocSize!=NULL);
-  KASSERT(nRamFrames>first);
+  if (!isTableActive())
+    return 0;
+  first = addr / PAGE_SIZE;
+  KASSERT(allocSize != NULL);
+  KASSERT(nRamFrames > first);
 
   spinlock_acquire(&freemem_lock);
-  for (i=first; i<first+np; i++) {
+  for (i = first; i < first + np; i++)
+  {
     freeRamFrames[i] = (unsigned char)1;
   }
   spinlock_release(&freemem_lock);
@@ -187,105 +203,102 @@ freeppages(paddr_t addr, unsigned long npages){
 vaddr_t
 alloc_kpages(unsigned npages)
 {
-	paddr_t pa;
+  paddr_t pa;
 
-	vm_can_sleep();
-	pa = getppages(npages);
-	if (pa==0) {
-		return 0;
-	}
-	return PADDR_TO_KVADDR(pa);
+  vm_can_sleep();
+  pa = getppages(npages);
+  if (pa == 0)
+  {
+    return 0;
+  }
+  return PADDR_TO_KVADDR(pa);
 }
 
-void 
-free_kpages(vaddr_t addr){
-  if (isTableActive()) {
+void free_kpages(vaddr_t addr)
+{
+  if (isTableActive())
+  {
     paddr_t paddr = addr - MIPS_KSEG0;
-    long first = paddr/PAGE_SIZE;	
-    KASSERT(allocSize!=NULL);
-    KASSERT(nRamFrames>first);
-    freeppages(paddr, allocSize[first]);	
+    long first = paddr / PAGE_SIZE;
+    KASSERT(allocSize != NULL);
+    KASSERT(nRamFrames > first);
+    freeppages(paddr, allocSize[first]);
   }
 }
-
-
 
 struct addrspace *
 as_create(void)
 {
-    struct addrspace *as;
+  struct addrspace *as;
 
-    as = kmalloc(sizeof(struct addrspace));
-    if (as == NULL)
-    {
-        return NULL;
-    }
+  as = kmalloc(sizeof(struct addrspace));
+  if (as == NULL)
+  {
+    return NULL;
+  }
 
-    as->as_vbase1=0;
-    as->as_vbase2=0;
+  as->as_vbase1 = 0;
+  as->as_vbase2 = 0;
 
-    return as;
-
+  return as;
 }
-
 
 int as_copy(struct addrspace *old, struct addrspace **ret)
 {
-    struct addrspace *newas;
+  struct addrspace *newas;
 
-    newas = as_create();
-    if (newas == NULL)
-    {
-        return ENOMEM;
-    }
+  newas = as_create();
+  if (newas == NULL)
+  {
+    return ENOMEM;
+  }
 
-    KASSERT(old != NULL);
-    KASSERT(old->as_vbase1 != 0); 
-    KASSERT(old->as_npages1 > 0); 
-    KASSERT(old->as_vbase2 != 0); 
-    KASSERT(old->as_npages2 > 0); 
+  KASSERT(old != NULL);
+  KASSERT(old->as_vbase1 != 0);
+  KASSERT(old->as_npages1 > 0);
+  KASSERT(old->as_vbase2 != 0);
+  KASSERT(old->as_npages2 > 0);
 
-    newas->as_vbase1 = old->as_vbase1;
-    newas->as_npages1 = old->as_npages1;
-    newas->as_vbase2 = old->as_vbase2;
-    newas->as_npages2 = old->as_npages2;
+  newas->as_vbase1 = old->as_vbase1;
+  newas->as_npages1 = old->as_npages1;
+  newas->as_vbase2 = old->as_vbase2;
+  newas->as_npages2 = old->as_npages2;
 
-    *ret = newas;
-    return 0;
+  *ret = newas;
+  return 0;
 }
 
- void
- as_destroy(struct addrspace *as)
- {
-     KASSERT(as != NULL);
-     kfree(as);
- }
-
-void
-as_activate(void)
+void as_destroy(struct addrspace *as)
 {
-	int i, spl;
-	struct addrspace *as;
-
-	as = proc_getas();
-	if (as == NULL) {
-		return;
-	}
-
-	/* Disable interrupts on this CPU while frobbing the TLB. */
-	spl = splhigh();
-
-	for (i=0; i<NUM_TLB; i++) {
-		tlb_write(TLBHI_INVALID(i), TLBLO_INVALID(), i);
-	}
-
-	splx(spl);
+  KASSERT(as != NULL);
+  kfree(as);
 }
 
-void
-as_deactivate(void)
+void as_activate(void)
 {
-	/*
+  int i, spl;
+  struct addrspace *as;
+
+  as = proc_getas();
+  if (as == NULL)
+  {
+    return;
+  }
+
+  /* Disable interrupts on this CPU while frobbing the TLB. */
+  spl = splhigh();
+
+  for (i = 0; i < NUM_TLB; i++)
+  {
+    tlb_write(TLBHI_INVALID(i), TLBLO_INVALID(), i);
+  }
+
+  splx(spl);
+}
+
+void as_deactivate(void)
+{
+  /*
 	 * Write this. For many designs it won't need to actually do
 	 * anything. See proc.c for an explanation of why it (might)
 	 * be needed.
@@ -302,68 +315,63 @@ as_deactivate(void)
  * moment, these are ignored. When you write the VM system, you may
  * want to implement them.
  */
-int
-as_define_region(struct addrspace *as, vaddr_t vaddr, size_t sz,
-		 int readable, int writeable, int executable)
+int as_define_region(struct addrspace *as, vaddr_t vaddr, size_t sz,
+                     int readable, int writeable, int executable)
 {
-	size_t npages;
+  size_t npages;
 
-	vm_can_sleep();
+  vm_can_sleep();
 
-	/* Align the region. First, the base... */
-	sz += vaddr & ~(vaddr_t)PAGE_FRAME;
-	vaddr &= PAGE_FRAME;
+  /* Align the region. First, the base... */
+  sz += vaddr & ~(vaddr_t)PAGE_FRAME;
+  vaddr &= PAGE_FRAME;
 
-	/* ...and now the length. */
-	sz = (sz + PAGE_SIZE - 1) & PAGE_FRAME;
+  /* ...and now the length. */
+  sz = (sz + PAGE_SIZE - 1) & PAGE_FRAME;
 
-	npages = sz / PAGE_SIZE;
+  npages = sz / PAGE_SIZE;
 
-	/* We don't use these - all text's segments are read-only and all the others read-write */
-	(void)readable;
-	(void)writeable;
-	(void)executable;
+  /* We don't use these - all text's segments are read-only and all the others read-write */
+  (void)readable;
+  (void)writeable;
+  (void)executable;
 
-	if (as->as_vbase1 == 0) {
-		as->as_vbase1 = vaddr;
-		as->as_npages1 = npages;
-		return 0;
-	}
+  if (as->as_vbase1 == 0)
+  {
+    as->as_vbase1 = vaddr;
+    as->as_npages1 = npages;
+    return 0;
+  }
 
-	if (as->as_vbase2 == 0) {
-		as->as_vbase2 = vaddr;
-		as->as_npages2 = npages;
-		return 0;
-	}
+  if (as->as_vbase2 == 0)
+  {
+    as->as_vbase2 = vaddr;
+    as->as_npages2 = npages;
+    return 0;
+  }
 
-	/*
+  /*
 	 * Support for more than two regions is not available.
 	 */
-	kprintf("dumbvm: Warning: too many regions\n");
-	return ENOSYS;
+  kprintf("dumbvm: Warning: too many regions\n");
+  return ENOSYS;
 }
 
-int
-as_prepare_load(unsigned npages)
+int as_prepare_load(unsigned npages)
 {
-	return getppages(npages);
+  return getppages(npages);
 }
 
-int
-as_complete_load(struct addrspace *as)
+int as_complete_load(struct addrspace *as)
 {
-	  vm_can_sleep();
-     (void)as;
-     return 0;
+  vm_can_sleep();
+  (void)as;
+  return 0;
 }
 
-
-
-int
-as_define_stack(struct addrspace *as, vaddr_t *stackptr)
+int as_define_stack(struct addrspace *as, vaddr_t *stackptr)
 {
-	KASSERT(as != NULL);
-	*stackptr = USERSTACK;
-	return 0;
+  KASSERT(as != NULL);
+  *stackptr = USERSTACK;
+  return 0;
 }
-
