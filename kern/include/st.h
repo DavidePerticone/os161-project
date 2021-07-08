4af1f33220986499eaae098d38f787047a160399
PIETRO MACORI s283421 <s283421@studenti.polito.it>
2021-07-08 12:00:46 +0200

    hash table implementation

---
+++ kern/include/st.h
@@ -0,0 +1,12 @@
+#ifndef _ST_H_
+#define _ST_H_
+
+typedef struct symboltable *ST;
+
+ST  	STinit(int) ;
+void 	STinsert(ST, Item) ;
+Item STsearch(ST, pid_t, vaddr_t);
+void	STdelete(ST, Key) ;
+void	STdisplay(ST) ;
+
+#endif
